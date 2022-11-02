885cfdcd7449370e31349a41947b886ad63c562f
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-11-02 10:55:39 +0300

    Update dropdown_button.dart

--- dropdown_button.dart
+++ dropdown_button.dart
@@ -12,6 +12,7 @@ class DropDownButton extends StatefulWidget {
 
 class _DropDownButtonState extends State<DropDownButton> {
   String? _selectedCity = null;
+  int? _selectedNum = null;
   List<String> _allCities = [
     'Ankara',
     'İstanbul',
@@ -20,54 +21,60 @@ class _DropDownButtonState extends State<DropDownButton> {
     'Adana',
     'Muğla',
   ];
+  List<int> _numbers = [1, 2, 3, 4, 5, 6];
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: DropdownButton<String>(
-        items: _allCities
-            .map(
-              (String citySelect) => DropdownMenuItem(
-                value: citySelect,
-                child: Text(citySelect),
-              ),
-            )
-            .toList(),
-        hint: Text('Select city'),
-        icon: Icon(Icons.arrow_drop_down_rounded),
-        underline: Container(
-          height: 2,
-          color: Colors.teal,
-        ),
-        style: TextStyle(color: Colors.teal, fontSize: 24),
-        value: _selectedCity,
-        onChanged: (String? _new) {
-          setState(() {
-            _selectedCity = _new;
-          });
-        },
-
-        /* items: [
-          DropdownMenuItem(
-            value: 01,
-            child: Text('Adana'),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DropdownButton<String>(
+            items: _allCities
+                .map(
+                  (String citySelect) => DropdownMenuItem(
+                    value: citySelect,
+                    child: Text(citySelect),
+                  ),
+                )
+                .toList(),
+            hint: Text('Select city'),
+            icon: Icon(Icons.arrow_drop_down_rounded),
+            underline: Container(
+              height: 2,
+              color: Colors.teal,
+            ),
+            style: TextStyle(color: Colors.teal, fontSize: 24),
+            value: _selectedCity,
+            onChanged: (String? _new) {
+              setState(() {
+                _selectedCity = _new;
+              });
+            },
           ),
-          DropdownMenuItem(
-            value: 06,
-            child: Text('Ankara'),
+          DropdownButton<int>(
+            items: _numbers
+                .map(
+                  (int citySelect) => DropdownMenuItem(
+                    value: citySelect,
+                    child: Text(citySelect.toString()),
+                  ),
+                )
+                .toList(),
+            hint: Text('Select number'),
+            icon: Icon(Icons.arrow_drop_down_rounded),
+            underline: Container(
+              height: 2,
+              color: Colors.teal,
+            ),
+            style: TextStyle(color: Colors.teal, fontSize: 24),
+            value: _selectedNum,
+            onChanged: (int? _new) {
+              setState(() {
+                _selectedNum = _new;
+              });
+            },
           ),
-          DropdownMenuItem(
-            value: 34,
-            child: Text('İstanbul'),
-          ),
-          DropdownMenuItem(
-            value: 35,
-            child: Text('İzmir'),
-          ),
-          DropdownMenuItem(
-            value: 16,
-            child: Text('Bursa'),
-          ),
-        ], */
+        ],
       ),
     );
   }
